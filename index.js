df95ebb1bf0113b764027f089e55a91a24b0c805
hazratgs <hazratgs@gmail.com>
2017-07-04 01:40:02 +0300

    Продажа btc после покупки

--- index.js
+++ index.js
@@ -45,23 +45,69 @@ const lastTransaction = async () => {
 
 // Наблюдение за ордерами
 const observeOrders = () => {
-  orders.map(async id => {
+  orders.map(async order => {
     try {
-      let res = await btce.orderInfo(id)
-      let info = res.return[id]
+      let res = await btce.orderInfo(order.id)
+      let info = res.return[order.id]
 
       // Оповещаем только о завершенных ордерах
       if (info.status === 1) return false
 
-      // Оповещаем пользователя о выполнении ордера
-      bot.sendMessage(config.user, `
-        💰 ${info.type === 'buy' ? 'Купили' : 'Продали'} (${id}) ${info.amount} BTC по курсу ${info.rate}
-      `)
+      if (info.type === 'buy') {
 
-      // Удаляем id из orders
-      orders.splice(orders.indexOf(id), 1)
+        // Оповещаем пользователя о купле
+        bot.sendMessage(config.user, `
+          💰 Купили ${info.amount} BTC по курсу ${info.rate}
+          order_id: ${order.id}
+        `)
+
+        // Выставляем на продажу ...
+        let buy = await btce.trade({
+          pair: config.pair,
+          type: 'sell',
+          rate: order.sell,
+          amount: config.amount
+        })
+
+        // Наблюдаем за ордером
+        orders.push({
+          id: buy.order_id,
+          price: order.sell,
+          sell: order.sell,
+          markup: config.markup
+        })
+
+        // Оповещаем пользователя о купле
+        bot.sendMessage(config.user, `
+          💰 Выставили на продажу ${info.amount} BTC по курсу ${info.rate}
+          order_id: ${buy.order_id}
+        `)
+
+      } else {
+
+        // Оповещаем о продаже
+        bot.sendMessage(config.user, `
+          🎉 Продали ${info.amount} BTC по курсу ${info.rate}
+          Дополнительно:
+          Объем: ${info.amount}
+          Купили: ${order.price}
+          Продали: ${order.sell} (${info.rate} по данным btc-e с учетом коммисии)
+          Наценка: ${order.markup}
+          Заработок: $${order.sell - order.price}
+          Коммисия: 0.4%
+          order_id: ${order.id}
+        `)
+      }
+
+      // Удаляем выполненный order из orders
+      for (let key in orders){
+        if (orders[key].id === order.id) {
+          orders.splice(key, 1)
+        }
+      }
     } catch (e) {
-      console.log(`Error observeOrders: ${e}`)
+      console.log(`Error observeOrders:`)
+      console.log(e)
     }
   })
 }
@@ -112,10 +158,10 @@ const trades = async () => {
   }
 }
 
-// Наблюдение за последними свечами, для выявления покупки или продажи
+// Наблюдение за последними свечами, для выявления покупки
 const observe = async () => {
   try {
-    if (!candles.length || candles.length < 10) {
+    if (!candles.length || candles.length < 240) {
       return false
     }
 
@@ -133,81 +179,81 @@ const observe = async () => {
     // Получаем последние свечи
     let data = candles.filter((item, index) => index <= 60)
 
+    // Текущая обстановка на рынке
+    let current = data.shift()
+
     // Последняя транзакция
     let lastTrade = await lastTransaction()
 
-    // Определяем
-    let type = lastTrade.type === 'buy' ? 'sell' : 'buy'
+    // Ожидаем, что последняя транзакция, это продажа
+    if (lastTrade.type === 'buy') {
+      return false
+    }
 
-    // Необходимо проанализировать данные и решить купить или продать
-    if (type === 'buy') {
+    // Поиск выгодного момента
+    for (let item of data){
+      if (current.price.min > item.price.min) {
+        // Не самая выгодная цена, сделка сорвана
+        return false
+      }
+    }
 
-      // Текущая обстановка на рынке
-      let current = data.shift()
+    // А так же проверяем, реально ли продать с 2% накидкой
+    let markupPrice = (current.price.min * (config.markup / 100)) + current.price.min
+    let markupPriceMin = null
+    let markupPriceMax = null
 
-      // Поиск выгодного момента
-      for (let item of data){
-        if (current.price.min > item.price.min) {
-          // Не самая выгодная цена, сделка сорвана
-          return false
-        }
+    let resolution = false
+
+    // Получаем необходимое количество свечей
+    let markupData = candles.filter((item, index) => index <= 720)
+    for (let item of markupData) {
+
+      // Если цена валюты достигала за последние n минут markupPrice
+      // то разрешаем покупать валюту
+      if (markupPrice <= item.price.max) {
+        resolution = true
       }
 
-      // А так же проверяем, реально ли продать с 2% накидкой
-      let markupPrice = (current.price.min * (config.markup / 100)) + current.price.min
-      let markupPriceMin = null
-      let markupPriceMax = null
+      markupPriceMin = markupPriceMin === null
+        ? item.price.min
+        : (markupPriceMin < item.price.min ? markupPriceMin : item.price.min)
 
-      let resolution = false
+      markupPriceMax = markupPriceMax === null
+        ? item.price.max
+        : (markupPriceMax > item.price.max ? markupPriceMax : item.price.max)
+    }
 
-      // Получаем необходимое количество свечей
-      let markupData = candles.filter((item, index) => index <= 720)
-      for (let item of markupData) {
+    if (resolution) {
+      // Покупаем
+      try {
+        let buy = await btce.trade({
+          pair: config.pair,
+          type: 'buy',
+          rate: current.price.min,
+          amount: config.amount
+        })
 
-        // Если цена валюты достигала за последние n минут markupPrice
-        // то разрешаем покупать валюту
-        if (markupPrice <= item.price.max) {
-          resolution = true
-        }
+        // Наблюдаем за ордером
+        orders.push({
+          id: buy.order_id,
+          price: current.price.min,
+          sell: markupPrice,
+          markup: config.markup
+        })
 
-        markupPriceMin = markupPriceMin === null
-          ? item.price.min
-          : (markupPriceMin < item.price.min ? markupPriceMin : item.price.min)
-
-        markupPriceMax = markupPriceMax === null
-          ? item.price.max
-          : (markupPriceMax > item.price.max ? markupPriceMax : item.price.max)
-      }
-
-      if (resolution) {
-        // Покупаем
-        try {
-          let buy = await btce.trade({
-            pair: config.pair,
-            type: type,
-            rate: current.price.min,
-            amount: config.amount
-          })
-
-          // Наблюдаем за ордером
-          orders.push(buy.order_id)
-
-          // Оповещаем об покупке
-          bot.sendMessage(config.user, `
+        // Оповещаем об покупке
+        bot.sendMessage(config.user, `
             ⌛ Запрос на покупку (${buy.order_id}) ${config.amount} BTC по курсу ${current.price.min}
             мин. цена: ${markupPriceMin}
             макс. цена: ${markupPriceMax}
             цена продажи: ${markupPrice}
           `)
 
-        } catch (e) {
-          console.log(`Buy error: ${e}`)
-          bot.sendMessage(config.user, `Ошибка buy: ${e}`)
-        }
+      } catch (e) {
+        console.log(`Buy error: ${e}`)
+        bot.sendMessage(config.user, `Ошибка buy: ${e}`)
       }
-
-    } else if (type === 'sell') {
-
     }
 
   } catch (e) {
@@ -219,7 +265,7 @@ const observe = async () => {
 setInterval(trades, 1000)
 
 // Наблюдение за ордерами
-// setInterval(observeOrders, 4000)
+setInterval(observeOrders, 4000)
 
 // Отслеживать каждую минуту ситуацию на рынке
-// setInterval(observe, 60000)
+setInterval(observe, 60000)
