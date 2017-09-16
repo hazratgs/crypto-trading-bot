7841280c690f7feaa39cc3f80d7be70a078a8629
hazratgs <hazratgs@gmail.com>
2017-09-16 23:56:41 +0300

    Добавлены дополнительные свойства

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  key: 'AH1VAUX4-V85UAXTF-PM3QCZPT-8H03DM5R-9263207H',
-  secret: 'a18dcaa951ea2248dee3b741a4bf52cfdd17b2a4745dc2b3aab6d933502d090c',
-  token: '373535924:AAHWTZ-5SnRQRgSP17R4PVjfJ1QZg4QvaC8',
-  user: 253178128,
+  key: '****',
+  secret: '****',
+  token: '****',
+  user: '',
   pair: 'btc_usd',
   markup: 0.3,
   amount: 0.24,
