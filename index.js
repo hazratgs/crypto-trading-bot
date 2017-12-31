3b0050944293043f8b7549f8640865764eb409f3
hazratgs <hazratgs@gmail.com>
2018-01-01 02:29:41 +0300

    Более правильное распределение средств

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Wex = require('./exchanges/wex')
 
 // Инициализация Wex ботов
-const WexBTC = new Wex({ pair: 'btc_usd', percentWallet: 33 })
-const WexETH = new Wex({ pair: 'eth_usd', percentWallet: 33 })
-const WexLTC = new Wex({ pair: 'ltc_usd', percentWallet: 33 })
+const WexBTC = new Wex({ pair: 'btc_usd', percentWallet: 40 })
+const WexETH = new Wex({ pair: 'eth_usd', percentWallet: 30 })
+const WexLTC = new Wex({ pair: 'ltc_usd', percentWallet: 30 })
 
 WexBTC.init()
 WexETH.init()
