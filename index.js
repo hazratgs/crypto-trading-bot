f8c4d59828137c011abe6d6ef6196ec6730fd59a
hazratgs <hazratgs@gmail.com>
2018-01-02 16:11:09 +0300

    Добавил рублевые операции

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const Wex = require('./exchanges/wex')
 
 // Расппеделение бюджета USD Wex
-const percentWalletUSD = ['btc', 'eth', 'ltc']
+const percentWalletUSD = ['btc', 'eth', 'ltc', 'dsh']
 
 // Расппеделение бюджета RUR Wex
-const percentWalletRUR = ['eth']
+const percentWalletRUR = ['btc', 'eth', 'ltc', 'dsh']
 
 // Инициализация Wex ботов
 const WexBTCUSD = new Wex({ pair: 'btc_usd', percentWallet: percentWalletUSD, commission: 0.2, markup: 1 })
 const WexLTCUSD = new Wex({ pair: 'ltc_usd', percentWallet: percentWalletUSD, commission: 0.2, markup: 1 })
 const WexETHUSD = new Wex({ pair: 'eth_usd', percentWallet: percentWalletUSD, commission: 0.2, markup: 1 })
-// const WexETHRUR = new Wex({ pair: 'eth_rur', percentWallet: percentWalletRUR, commission: 0.2, markup: 1 })
+const WexDSHUSD = new Wex({ pair: 'dsh_usd', percentWallet: percentWalletUSD, commission: 0.2, markup: 1 })
 
+// Рублевые
+const WexBTCRUR = new Wex({ pair: 'btc_rur', percentWallet: percentWalletRUR, commission: 0.2, markup: 1 })
+const WexETHRUR = new Wex({ pair: 'eth_rur', percentWallet: percentWalletRUR, commission: 0.2, markup: 1 })
+const WexLTCRUB = new Wex({ pair: 'ltc_rur', percentWallet: percentWalletRUR, commission: 0.2, markup: 1 })
+const WexDSHRUR = new Wex({ pair: 'dsh_rur', percentWallet: percentWalletRUR, commission: 0.2, markup: 1 })
+
+// Старт ботов
 WexBTCUSD.init()
 WexLTCUSD.init()
 WexETHUSD.init()
-// WexETHRUR.init()
+WexDSHUSD.init()
+WexETHRUR.init()
+WexBTCRUR.init()
+WexLTCRUB.init()
+WexDSHRUR.init()
